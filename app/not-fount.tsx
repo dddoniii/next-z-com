4a1c7369f2026a0bcdb401ef03e111eb6bbfd369
eunjin <eunjin9487@gmail.com>
2025-02-20 18:01:26 +0900

    tweet 페이지 개발

--- app/not-fount.tsx
+++ app/not-fount.tsx
@@ -1,11 +1,11 @@
-import { NextPage } from "next"
-import Link from "next/link"
+import { NextPage } from "next";
+import Link from "next/link";
 
 const NotFount: NextPage = () => {
-    return (
-        <div>
-            <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
-            <Link href="/serch">검색</Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
+      <Link href="/serch">검색</Link>
+    </div>
+  );
+};
