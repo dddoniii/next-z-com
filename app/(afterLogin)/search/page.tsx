51701baff80486f50db66a7c13942cb35e4c748a
eunjin <eunjin9487@gmail.com>
2025-02-21 16:43:14 +0900

    photo 페이지 개발까지

--- app/(afterLogin)/search/page.tsx
+++ app/(afterLogin)/search/page.tsx
@@ -1,3 +1,42 @@
-export default function Home() {
-  return <div>검색 페이지</div>;
+import BackButton from "../_component/BackButton";
+import SearchForm from "../_component/SearchForm";
+import Post from "../_component/Post";
+import style from "./search.module.css";
+import Tab from "./Tab";
+
+type Props = {
+  searchParams: Promise<{ q: string; f?: string; pf?: string }>;
+};
+
+export default async function Search({ searchParams }: Props) {
+  const { q } = await searchParams;
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
