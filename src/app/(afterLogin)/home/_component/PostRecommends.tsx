73cc9423b0a63a1243050ea5fe5fb1967807cc05
eunjin <eunjin9487@gmail.com>
2025-03-04 18:24:59 +0900

    explore 페이지

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -9,8 +9,8 @@ export default function PostRecommends() {
   const { data } = useQuery<IPost[]>({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
-    staleTime: 60 * 1000, // fresh -> stale time, 5분이라는 기준, 항상 gcTime보다 짧아야한다
-    gcTime: 300 * 1000, // 가비지 콜렉터 타임(사용하지 않는 데이터 inactive 정리 주기), 기본 5분
+    staleTime: 60 * 1000,
+    gcTime: 300 * 1000,
   });
 
   return data?.map((post) => <Post key={post.postId} post={post} />);
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,5 +1,14 @@
+interface UserID {
+  id: string;
+}
+
 export interface User {
   id: string;
   nickname: string;
-  image: Date;
+  image: string;
+  Followers: UserID[];
+  _count: {
+    Followers: number;
+    Followings: number;
+  };
 }
